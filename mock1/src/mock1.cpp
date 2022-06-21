e50c42f090c570c2ba1d03cb0d81dfacb76a01ab
alpylmz <ylmz.alp.e@gmail.com>
2022-06-21 14:53:56 +0300

    node 1 now repeats the same message until the first message is got

--- mock1/src/mock1.cpp
+++ mock1/src/mock1.cpp
@@ -4,12 +4,16 @@
 // This is only true if the executable is mock1
 bool took_message = true;
 
+bool took_any_message = false;
+
+
 // A subscriber to topic3 that will take the message, and if it is "Hello World", makes the took_message variable true
 void callback(const std_msgs::msg::String::SharedPtr msg)
 {
     // if msg-> data starts with "Hello World"
     if (msg->data.substr(0, 11) == "Hello World") {
         took_message = true;
+        took_any_message = true;
     }
 }
 
@@ -35,6 +39,14 @@ int main(int argc, char ** argv)
             loop_rate.sleep();
             took_message = false;
             i++;
+        }
+        else if(!took_any_message) {
+            RCLCPP_INFO(node->get_logger(), "I didn't take any messages!");
+            auto msg = std_msgs::msg::String();
+            msg.data = "Hello World" + std::to_string(i);
+            publisher->publish(msg);
+            rclcpp::spin_some(node);
+            loop_rate.sleep();
         }
         else{
             rclcpp::spin_some(node);
